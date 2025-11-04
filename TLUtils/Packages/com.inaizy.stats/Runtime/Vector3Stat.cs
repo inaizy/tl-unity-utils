9d8011ace4c3a17bed60f5acc6d838aa11ad4441
theresa latzko <inai.zy@gmail.com>
2025-11-03 17:32:32 -0800

    added vector3

---
+++ TLUtils/Packages/com.inaizy.stats/Runtime/Vector3Stat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace TLUtils
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects / Stat / Vector3")]
+    public class Vector3Stat : Stat<Vector3>
+    {
+
+        public override void Reset()
+        {
+            _value = _defaultValue;
+            ValueChanged?.Invoke();
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString("F3");
+        }
+    }
+}
